74297b17f72f634ba95e13fa04158b5b8b33fc26
hüseyin tugay yeşilyurt <tugayesilyurt@huseyin-Air.home>
2023-12-02 12:42:38 +0300

    Initialize Online Coffee Store Backend API Project

---
+++ coffee-store-backend/src/main/java/com/bestseller/coffee/CoffeeStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bestseller.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeStoreBackendApplication.class, args);
+	}
+
+}
